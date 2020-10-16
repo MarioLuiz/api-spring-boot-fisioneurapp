3f7529c13915fe7f2833ab2ba03286023663a982
Mario Luiz <marioluiz.arruda@gmail.com>
2020-10-15 21:57:36 -0400

    Criado FisioterapeutaRepository

---
+++ fisioneuapp/src/main/java/com/arrudamoreira/fisioneuapp/repositories/FisioterapeutaRepository.java
@@ -0,0 +1,14 @@
+package com.arrudamoreira.fisioneuapp.repositories;
+
+import com.arrudamoreira.fisioneuapp.domain.Fisioterapeuta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Mario Arruda
+ */
+@Repository
+public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, Integer>{
+    
+}
