8d852161758d2d192a9c0d0e07e887385072c6b0
MarioLuiz <marioluiz.arruda@gmail.com>
2020-11-11 14:05:10 -0400

    Adicionado excecao personalizada para Objeto nao encontrado

---
+++ src/main/java/com/arrudamoreira/fisioneurapp/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.arrudamoreira.fisioneurapp.services.exceptions;
+
+/**
+ *
+ * @author marruda
+ */
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public ObjectNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
