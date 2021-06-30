ced65a8e6d865cd8ff67e9c75e6c649e778c7cc9
MarioLuiz <marioluiz.arruda@gmail.com>
2021-06-30 14:38:27 -0400

    Implementando autorização token JWT

--- src/main/java/com/arrudamoreira/fisioneurapp/config/SecurityConfig.java
+++ src/main/java/com/arrudamoreira/fisioneurapp/config/SecurityConfig.java
@@ -19,62 +19,60 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.arrudamoreira.fisioneurapp.security.JWTAuthenticationFilter;
+import com.arrudamoreira.fisioneurapp.security.JWTAuthorizationFilter;
 import com.arrudamoreira.fisioneurapp.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-    @Autowired
-    private Environment env;
-    
-    @Autowired
-    private JWTUtil jwtUtil;
 
-    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+	@Autowired
+	private Environment env;
 
-    private static final String[] PUBLIC_MATCHERS_GET = {
-        //"/fisioterapeutas/**"
-    };
-    
-    private static final String[] PUBLIC_MATCHERS_POST = {
-        "/fisioterapeutas/**"
-    };
+	@Autowired
+	private JWTUtil jwtUtil;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-            http.headers().frameOptions().disable();
-        }
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			// "/fisioterapeutas/**"
+	};
 
-        http.cors().and().csrf().disable();
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                .antMatchers(PUBLIC_MATCHERS).permitAll()
-                .anyRequest().authenticated();
-        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-    
-    @Override
+	private static final String[] PUBLIC_MATCHERS_POST = { "/fisioterapeutas/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
+		http.cors().and().csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
